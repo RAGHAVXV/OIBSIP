299b6d171f6444d15b84cbf8584a05e30b83d66e
RAGHAV <raghavoppari7@gmail.com>
2026-08-15 00:01:21 +0530

    Add Temperature Converter submission

--- WebDev-L1-TemperatureConverter/script.js
+++ WebDev-L1-TemperatureConverter/script.js
@@ -12,39 +12,46 @@ btn.addEventListener("click", function () {
 
     let temp = parseFloat(input.value);
 
-    // Check for empty or invalid input
     if (isNaN(temp)) {
         error.innerText = "Please enter a valid temperature.";
+
         celsius.innerText = "-- °C";
         fahrenheit.innerText = "-- °F";
         kelvin.innerText = "-- K";
+
         return;
     }
+
     let c, f, k;
 
     if (unit.value === "celsius") {
+
         c = temp;
         f = (temp * 9 / 5) + 32;
         k = temp + 273.15;
-    }
 
-    else if (unit.value === "fahrenheit") {
+    } else if (unit.value === "fahrenheit") {
+
         c = (temp - 32) * 5 / 9;
         f = temp;
         k = c + 273.15;
-    }
 
-    else {
+    } else if (unit.value === "kelvin") {
+
         c = temp - 273.15;
         f = (c * 9 / 5) + 32;
         k = temp;
     }
 
     if (c < -273.15) {
-        error.innerText = "Temperature cannot be below absolute zero.";
+
+        error.innerText =
+            "Temperature cannot be below absolute zero.";
+
         celsius.innerText = "-- °C";
         fahrenheit.innerText = "-- °F";
         kelvin.innerText = "-- K";
+
         return;
     }
 
